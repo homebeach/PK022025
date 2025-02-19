722d24c1c1c3f83f3d20b18f6dfba3e563abc21e
Petri Kotiranta <petri.kotiranta@exaum.com>
2025-02-19 06:18:53 +0200

    Dialog menu added

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Drawer, List, ListItem, ListItemText, IconButton, Menu, MenuItem, Button } from "@mui/material";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../store/store";
 import { deleteChart } from "../store/chartSlice";
+import AddChartDialog from "./AddChartDialog"; // Import the modal component
 
 const Sidebar: React.FC = () => {
   const charts = useSelector((state: RootState) => state.charts.charts);
   const dispatch = useDispatch();
 
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const [selectedChart, setSelectedChart] = React.useState<number | null>(null);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [selectedChart, setSelectedChart] = useState<number | null>(null);
+  const [isDialogOpen, setIsDialogOpen] = useState(false); // State to control the modal
 
   const handleMenuOpen = (event: React.MouseEvent<HTMLButtonElement>, chartId: number) => {
     setAnchorEl(event.currentTarget);
@@ -35,32 +37,42 @@ const Sidebar: React.FC = () => {
   };
 
   return (
-    <Drawer variant="permanent" anchor="left">
-      <div style={{ padding: 16 }}>
-        <Button variant="contained" fullWidth>+ ADD CHART</Button>
-      </div>
-      <List>
-        {charts.length === 0 ? (
-          <ListItem>
-            <ListItemText primary="No charts" />
-          </ListItem>
-        ) : (
-          charts.map((chart) => (
-            <ListItem key={chart.id} secondaryAction={
-              <IconButton onClick={(event) => handleMenuOpen(event, chart.id)}>
-                <MoreVertIcon />
-              </IconButton>
-            }>
-              <ListItemText primary={chart.name} />
+    <>
+      <Drawer variant="permanent" anchor="left">
+        <div style={{ padding: 16 }}>
+          <Button variant="contained" fullWidth onClick={() => setIsDialogOpen(true)}>
+            + ADD CHART
+          </Button>
+        </div>
+        <List>
+          {charts.length === 0 ? (
+            <ListItem>
+              <ListItemText primary="No charts" />
             </ListItem>
-          ))
-        )}
-      </List>
-      <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
-        <MenuItem onClick={handleEdit}>Edit</MenuItem>
-        <MenuItem onClick={handleDelete}>Delete</MenuItem>
-      </Menu>
-    </Drawer>
+          ) : (
+            charts.map((chart) => (
+              <ListItem
+                key={chart.id}
+                secondaryAction={
+                  <IconButton onClick={(event) => handleMenuOpen(event, chart.id)}>
+                    <MoreVertIcon />
+                  </IconButton>
+                }
+              >
+                <ListItemText primary={chart.name} />
+              </ListItem>
+            ))
+          )}
+        </List>
+        <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
+          <MenuItem onClick={handleEdit}>Edit</MenuItem>
+          <MenuItem onClick={handleDelete}>Delete</MenuItem>
+        </Menu>
+      </Drawer>
+
+      {/* Add Chart Modal */}
+      <AddChartDialog open={isDialogOpen} onClose={() => setIsDialogOpen(false)} />
+    </>
   );
 };
 
