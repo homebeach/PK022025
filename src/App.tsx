d32fa1aded7f9d245b36180cae289303168a427a
Petri Kotiranta <petri.kotiranta@exaum.com>
2025-02-19 04:50:14 +0200

    Project fixed after transition to TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React from "react";
 import Sidebar from "./components/Sidebar";
 import "./App.css";
 
-const App: React.FC = () => {
+const App = () => {
   return (
     <div className="App" style={{ display: "flex" }}>
       <Sidebar />
